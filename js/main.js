60c7c1c657ed5722dd120004266c4a01221dacba
MA <marreguin@wisc.edu>
2021-01-19 22:11:22 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2541,7 +2541,7 @@ var backgroundLayerOSM = new L.tileLayer('https://cartodb-basemaps-{s}.global.ss
 var backgroundLayerMB = new L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.streets',
+    id: 'mapbox.outdoors',
     accessToken: 'pk.eyJ1IjoibWFycmVndWluIiwiYSI6ImNrNmU1MHlhZzEyaXYza3MwNjZhZ3RudGMifQ.7Z4ngmNwEuV5GJ-wDT15nA'
 }).addTo(map);
 
